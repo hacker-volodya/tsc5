bb1055a05ac6b1aab938a3db754db240fca53710
Vladimir Lebedev <d3fl4t3@gmail.com>
2023-12-31 00:11:41 +0700

    Accept, add tests

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -1,5 +1,5 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import { Address, Cell, toNano } from 'ton-core';
 import { Task1 } from '../wrappers/Task1';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
@@ -17,7 +17,12 @@ describe('Task1', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        task1 = blockchain.openContract(Task1.createFromConfig({}, code));
+        task1 = blockchain.openContract(Task1.createFromConfig({
+            publicKey: '06aa52e6f0324fa8fb7e5a2a41153264aea2901de57a2c3d72e151e0f74ef01f',
+            executionTime: 123,
+            receiver: Address.parseFriendly("EQC38-cbo1HivDOdH0oOzyZfTKVpSkatn1ydXJYsrg5KvLNI").address,
+            seqno: 0
+        }, code));
 
         const deployer = await blockchain.treasury('deployer');
 
@@ -34,5 +39,14 @@ describe('Task1', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and task1 are ready to use
+    });
+
+    it('should claim', async () => {
+        const res = await task1.sendClaim();
+        
+        expect(res.transactions).toHaveTransaction({
+            from: task1.address,
+            to: Address.parseFriendly("EQC38-cbo1HivDOdH0oOzyZfTKVpSkatn1ydXJYsrg5KvLNI").address
+        })
     });
 });
--- wrappers/Task1.ts
+++ wrappers/Task1.ts
@@ -1,9 +1,19 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type Task1Config = {};
+export type Task1Config = {
+    publicKey: string,
+    executionTime: number,
+    receiver: Address,
+    seqno: number,
+};
 
 export function task1ConfigToCell(config: Task1Config): Cell {
-    return beginCell().endCell();
+    return beginCell()
+        .storeBuffer(Buffer.from(config.publicKey, 'hex'))
+        .storeUint(config.executionTime, 32)
+        .storeAddress(config.receiver)
+        .storeUint(config.seqno, 32)
+        .endCell();
 }
 
 export class Task1 implements Contract {
@@ -25,5 +35,13 @@ export class Task1 implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
+    }
+
+    async sendUpdate(provider: ContractProvider, signature: string, lockedFor: number, newSeqno: number) {
+        await provider.external(beginCell().storeUint(0x9df10277, 32).storeUint(0, 64).storeBuffer(Buffer.from(signature, 'hex')).storeRef(beginCell().storeUint(lockedFor, 32).storeUint(newSeqno, 32).endCell()).endCell())
+    }
+
+    async sendClaim(provider: ContractProvider) {
+        await provider.external(beginCell().storeUint(0xbb4be234, 32).storeUint(0, 64).endCell())
     }
 }
